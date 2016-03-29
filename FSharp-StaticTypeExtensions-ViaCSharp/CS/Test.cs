8045153b38dff08997292c1ee433c2c1374360fe
Kenneth Ito <kenneth.ito@gmail.com>
2016-03-29 15:39:57 -0700

    Add static extensions example

---
+++ FSharp-StaticTypeExtensions-ViaCSharp/CS/Test.cs
@@ -0,0 +1,37 @@
+﻿using FS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS
+{
+    public class Test
+    {
+        public void Caller()
+        {
+            TestType.IrrelevantFunction();
+            
+            //We want to call this
+            //TestType.NeedToCallThis();
+
+            //Metadata:
+
+            //namespace FS
+            //{
+            //    [Microsoft.FSharp.Core.AutoOpenAttribute]
+            //    [Microsoft.FSharp.Core.CompilationMappingAttribute]
+            //    public static class Extensions
+            //    {
+            //        public static int TestType.NeedToCallThis.Static();
+            //    }
+            //}
+
+            //None of these compile
+            //TestType.NeedToCallThis();
+            //Extensions.TestType.NeedToCallThis.Static();
+            //Extensions.TestType.NeedToCallThis();
+        }
+    }
+}
---
+++ FSharp-StaticTypeExtensions-ViaCSharp/FS/Test.fs
@@ -0,0 +1,18 @@
+﻿namespace FS
+
+[<AbstractClass; Sealed>]
+type TestType() = 
+    static member IrrelevantFunction() = 
+        0
+
+[<AutoOpen>]
+module Extensions = 
+    type TestType with
+        //How do we call this from C#
+        static member NeedToCallThis() = 
+            0
+
+module Caller = 
+    let CallIt() = 
+        //F# can call it
+        TestType.NeedToCallThis()
