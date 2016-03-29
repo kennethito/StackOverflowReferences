bc57abf3a4085835cea521fec44dec99f40c13f4
Kenneth Ito <kenneth.ito@gmail.com>
2016-03-29 13:28:24 -0700

    Initial commit

---
+++ FSharpValue-GetUnionFields/CsharpPortable/Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.FSharp.Core;
+using Microsoft.FSharp.Reflection;
+
+namespace CsharpPortable
+{
+    static class Test
+    {
+        static FSharpOption<int> TestIt()
+        {
+            var option = new FSharpOption<int>(123);
+
+            //Does not compile, however the F# version does
+            return FSharpValue.GetUnionFields(option, option.GetType());
+        }
+    }
+}
---
+++ FSharpValue-GetUnionFields/FsharpPortable/Test.fs
@@ -0,0 +1,8 @@
+﻿module Test
+
+open Microsoft.FSharp.Reflection
+
+let TestIt() = 
+    let option = Some(123)
+    FSharpValue.GetUnionFields(option, option.GetType())
+
